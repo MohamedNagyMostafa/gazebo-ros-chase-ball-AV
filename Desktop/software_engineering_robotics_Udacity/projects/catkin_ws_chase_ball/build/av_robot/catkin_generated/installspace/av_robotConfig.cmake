c2f8e306bec2f9280901852bba1b9967d38fb225
MohamedNagyMostafa <mohamednagy2015@outlook.com>
2023-06-14 15:01:57 +0400

    add camera sensor

--- Desktop/software_engineering_robotics_Udacity/projects/catkin_ws_chase_ball/build/av_robot/catkin_generated/installspace/av_robotConfig.cmake
+++ Desktop/software_engineering_robotics_Udacity/projects/catkin_ws_chase_ball/build/av_robot/catkin_generated/installspace/av_robotConfig.cmake
@@ -154,7 +154,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /home/nagy/Desktop/software_engineering_robotics_Udacity/projects/catkin_ws_chase_ball/install/lib;/home/nagy/Desktop/software_engineering_robotics_Udacity/projects/catkin_ws_arm/devel/lib;/home/nagy/catkin_ws/devel/lib;/opt/ros/noetic/lib)
+    foreach(path /home/nagy/Desktop/software_engineering_robotics_Udacity/projects/catkin_ws_chase_ball/install/lib;/home/nagy/catkin_ws/devel/lib;/opt/ros/noetic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)

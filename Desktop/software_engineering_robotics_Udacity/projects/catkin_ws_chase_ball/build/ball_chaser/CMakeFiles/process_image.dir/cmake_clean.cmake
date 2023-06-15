ec37d8c3714acd504f559eafc25867d026eaec01
MohamedNagyMostafa <mohamednagy2015@outlook.com>
2023-06-15 16:05:35 +0400

    ball cheaser completed

---
+++ Desktop/software_engineering_robotics_Udacity/projects/catkin_ws_chase_ball/build/ball_chaser/CMakeFiles/process_image.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/nagy/Desktop/software_engineering_robotics_Udacity/projects/catkin_ws_chase_ball/devel/lib/ball_chaser/process_image"
+  "/home/nagy/Desktop/software_engineering_robotics_Udacity/projects/catkin_ws_chase_ball/devel/lib/ball_chaser/process_image.pdb"
+  "CMakeFiles/process_image.dir/src/process_image.cpp.o"
+  "CMakeFiles/process_image.dir/src/process_image.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/process_image.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Desktop/software_engineering_robotics_Udacity/projects/catkin_ws_chase_ball/src/ball_chaser/src/process_image.cpp
@@ -0,0 +1,92 @@
+//
+// Created by nagy on 6/15/23.
+//
+#include "ros/ros.h"
+#include "ball_chaser/DriveToTarget.h"
+#include <sensor_msgs/Image.h>
+
+ros::ServiceClient driveRobotClient;
+
+void driveRobot(float linearX, float angularZ)
+{
+    ball_chaser::DriveToTarget srv;
+    srv.request.linear_x    = linearX;
+    srv.request.angular_z   = angularZ;
+
+    if(!driveRobotClient.call(srv))
+        ROS_ERROR("Failed to call service drive_robot");
+}
+
+void processImageCallback(const sensor_msgs::Image image)
+{
+    int whitePixel  = 255;
+
+    int leftMovementTolerance       = image.width/3;
+    int forwardMovementTolerance    = leftMovementTolerance + image.width/3;
+
+    std::vector<int8_t> directionDecision = {0, 0, 0};
+    int maxDirectionPro = -1;
+
+    for(int i =0; i < image.height; i++)
+    {
+        for(int j=0; j < image.width; j++)
+        {
+            int pixelValue  = image.data[i * image.step + (j * image.step/image.width)];
+            if(pixelValue == whitePixel)
+            {
+                if(j < leftMovementTolerance)
+                {
+                    directionDecision[0]++;
+                    maxDirectionPro = (directionDecision[0] > maxDirectionPro)? 0 : maxDirectionPro;
+                }
+
+                else if(j < forwardMovementTolerance)
+                {
+                    directionDecision[1]++;
+                    maxDirectionPro = (directionDecision[1] > maxDirectionPro)? 1 : maxDirectionPro;
+                }
+                else
+                {
+                    directionDecision[2]++;
+                    maxDirectionPro = (directionDecision[2] > maxDirectionPro)? 2 : maxDirectionPro;
+                }
+            }
+        }
+    }
+    switch(maxDirectionPro)
+    {
+        case 0:
+            driveRobot(0, 0.1);
+            ROS_INFO("Robot is turning left");
+            break;
+        case 1:
+            driveRobot(0.3, 0);
+            ROS_INFO("Robot is moving forward");
+            break;
+        case 2:
+            driveRobot(0.0, -.1);
+            ROS_INFO("Robot is turning right");
+            break;
+        default:
+            driveRobot(0.0, 0.0);
+            ROS_INFO("Robot is stopped");
+    }
+
+}
+
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "process_image");
+    ros::NodeHandle nodeHandle;
+
+    driveRobotClient = nodeHandle.serviceClient<ball_chaser::DriveToTarget>("/ball_chaser/command_robot");
+
+    ros::Subscriber cameraSubscriber    = nodeHandle.subscribe("/camera/rgb/image_raw", 10, processImageCallback);
+
+    ros::spin();
+
+    return 0;
+
+}
+
